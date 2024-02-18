a51c835cd5f8885405e7788cb6b633c7a90389de
MINHAJ BHM <MINHAJ BHM@DESKTOP-2H9UO1V>
2024-02-18 08:57:37 +0530

    Add project files.

---
+++ Super Market Management System/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Super_Market_Management_System.Models;
+
+namespace Super_Market_Management_System.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryID = id.HasValue ? id.Value : 0 };
+
+            return View(category);
+        }
+    }
+}
---
+++ Super Market Management System/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Super_Market_Management_System.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
